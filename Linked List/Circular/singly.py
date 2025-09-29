1f7d694347477670bab2cae8052120350eb39c2f
Nishan <aanishan339@gmail.com>
2025-09-29 19:05:46 +0600

    Optimized circular singly ds

--- Linked List/Circular/singly.py
+++ Linked List/Circular/singly.py
@@ -6,6 +6,7 @@ class Node:
 class Circular:
     def __init__(self):
         self.head = None
+        self.tail = None
     
     def isEmpty(self):
         return self.head == None
@@ -17,149 +18,131 @@ class Circular:
             temp = self.head
             count = 1
             while temp.next != self.head:
-                temp = temp.next
+                temp = temp.next 
                 count += 1
             return count
-
-    def lastnode(self):
-        temp = self.head
-        while temp.next != self.head:
-            temp = temp.next
-        return temp
     
-    # InsertFirst
+    # Insertion method
     def insertFirst(self,item):
         node = Node(item)
         if self.isEmpty():
             self.head = node
-            node.next = self.head
+            self.tail = node
         else:
-            lastnode = self.lastnode()
-            lastnode.next = node
             node.next = self.head
             self.head = node
-    
+        self.tail.next = node
+
     def insertEnd(self,item):
         if self.isEmpty():
             self.insertFirst(item)
         else:
             node = Node(item)
-            lastnode = self.lastnode()
-            lastnode.next = node
             node.next = self.head
-    
+            self.tail.next = node
+            self.tail = node
+
     def insertAt(self,pos,item):
         length = self.length()
-        if pos < 1 or pos > length:
+        if pos < 1 or pos > length+1:
             print('Invalid position')
         elif pos == 1:
             self.insertFirst(item)
+        elif pos == length+1:
+            self.insertEnd(item)
         else:
-            temp = self.head
             node = Node(item)
+            temp = self.head
             i = 1
             while i < pos - 1:
                 temp = temp.next
                 i += 1
-                if temp.next == self.head:
-                    break
             target = temp.next
-            temp.next = node
             node.next = target
+            temp.next = node
 
-    # Deletion
+    # Deletion meethods
     def deleteFirst(self):
         if self.isEmpty():
             print('The Linked List is empty')
-        elif self.length() == 1:
+        elif self.head.next == self.head:
             temp = self.head
-            self.head = None
+            self.head = None 
+            self.tail = None
             del temp
         else:
             temp = self.head
-            nextnode = temp.next
-            lastnode = self.lastnode()
-            self.head = nextnode
-            lastnode.next = self.head
+            self.tail.next = temp.next
+            self.head = temp.next
             del temp
     
     def deleteEnd(self):
-        if self.isEmpty():
-            print('The Linked List is empty')
-        elif self.length() == 1:
+        if self.isEmpty() or self.head.next == self.head:
             self.deleteFirst()
         else:
             temp = self.head
-            lastnode = self.lastnode()
-            prev = self.length() - 1
-            i = 1
-            while i < prev:
+            lastnode = self.tail
+            while temp.next.next != self.head:
                 temp = temp.next
-                i += 1
             temp.next = self.head
+            self.tail = temp
             del lastnode
 
     def deleteAt(self,pos):
         length = self.length()
-        if self.isEmpty():
-            print('The Linked List is empty')
-        elif pos < 1 or pos > length:
+        if pos < 1 or pos > length:
             print('Invalid position')
         elif pos == 1:
             self.deleteFirst()
         elif pos == length:
             self.deleteEnd()
         else:
-            pos = pos - 1
+            pos = pos -1
             i = 1
-            prev = self.head
+            temp = self.head
             while i < pos:
-                prev = prev.next
+                temp = temp.next
                 i += 1
-            target = prev.next
-            nextnode = target.next 
-            prev.next = nextnode
+            target = temp.next
+            temp.next = target.next 
             del target
-        
+
     def display(self):
         if self.isEmpty():
             print('The Linked List is empty')
-        temp = self.head
-        while temp:
-            print(temp.item,end="->")
-            temp = temp.next
-            if temp == self.head:
-                break
-    
+        else:
+            temp = self.head 
+            while temp.next != self.head:
+                print(temp.item,end="->")
+                temp = temp.next
+            print(temp.item) 
 
 sll = Circular()
-
 # Checking insertFirst() method
 for i in range(10):
     sll.insertFirst(i)
 
+# Checking insertEnd() method
+for i in range(20 , 11 , -1):
+    sll.insertEnd(i)
+
 # Checking insertAt() method
-sll.insertAt(1,110)
-sll.insertAt(11,111)
-sll.insertAt(12,113)
+sll.insertAt(1,13)
+sll.insertAt(20,14)
+sll.insertAt(5,15)
 
 # Checking deleteFirst() method
-for i in range(13):
+for i in range(5):
     sll.deleteFirst()
 
 # Checking deleteEnd() method
-for i in range(10):
+for i in range(5):
     sll.deleteEnd()
 
 # Checking deleteAt() method
-sll.deleteAt(5)
 sll.deleteAt(1)
 sll.deleteAt(11)
-sll.deleteAt(10)
+sll.deleteAt(6)
 # Checking display() method
 sll.display()
-            
-
-
-
-
+print(f'\nHead:{sll.head.item}\nTail:{sll.tail.item}--Next-->{sll.tail.next.item}\nLength:{sll.length()}')
